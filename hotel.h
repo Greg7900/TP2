08a03916a17072fee1884995d4e346ea59a07b23
Gregory <torch28@hotmail.fr>
2021-12-08 16:49:37 +0100

    dev q4

--- hotel.h
+++ hotel.h
@@ -1,27 +1,29 @@
 #ifndef HOTEL_H
 #define HOTEL_H
 #include <iostream>
-#include <array>
+#include <vector>
 
 class Hotel {
 
 public:
-   Hotel(std::string idUnique, std::string nom, std::string ville, int nombredechambre );
+   Hotel(std::string idUnique, std::string nom, std::string ville);
    std::string  idUnique() const;
    std::string  nom() const;
    std::string ville() const;
-   int nombredechambre() const;
+   int NbChambre() const;
 
    void updateIdUnique(std::string idUnique);
    void updateNom(std::string nom);
    void updateVille(std::string ville);
-   void updateNbChambre(int nombredechambre);
+   void updateNbChambre(int nbChambre);
   
 private:
    std::string _idUnique;
    std::string _nom;
    std::string _ville;
    int _nombredechambre;
+   std::vector<std::string> _Chambre;
+   
 
 
 
